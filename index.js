7bf2978e00691798d1c357062e0a8b05fffda43c
Conor Meldrum <conor.j.meldrum@gmail.com>
2022-09-13 17:06:37 -0400

    adds source files

---
+++ index.js
@@ -0,0 +1,5 @@
+import { ... } from 'espn-fantasy-football-api';
+
+const myClient = new Client({ leagueId: 1991339 });
+myClient.setCookies({ espnS2: 'AEBHanwp0Ow2%2BbwUY%2Bhy504hTikkXwUDxifyn70FRDBLKsWsQevjkesB1KVcRwAJBLZaNOi5USooe2GBpsjlNeagMdMdkR2rv08AWgQmEfa7QKi8aK%2BwuQK4BWPB%2B4Hli9ugX3GEOBqj4GubfM41eZ%2B%2Ftfsryd94eRSYQpW%2BYmZEexg7sO3TaV8L2pOF8k%2BLKZKFaeX7RaweVJ7eWbO%2BDwrniO6KhOxVgLB7usY5Se9FErUNFYLqmtA6fdiBqHFww1926dEGzs75uwVKs5t3keZa', SWID: '6DD65B80-9214-4463-965B-809214F46354' });
+
